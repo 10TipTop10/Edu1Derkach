1769cf91ae4509ea089729ec7821f2902a285a61
user <user@DESKTOP-L59GEEH>
2023-05-30 20:26:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Programm
+{
+    static void Main(string[] args)
+    {
+        //Новая строка кода не добавляет новую строку в консоли.
+        Console.Write("Привет Мир!!!");
+
+        //Переход на новую строку.
+        Console.WriteLine();
+
+        //Каждая строка кода отображается в той же строке консоли.
+        Console.Write("Привет ");
+        Console.Write("Мир");
+        Console.Write("!!!");
+
+        //Переход на новую строку.
+        Console.WriteLine();
+
+        //Строка в коде использует новую строку в консоли.
+        Console.WriteLine("Привет Мир!!!");
+
+        //Каждая новая строка кода выводится с новой строки в консоли.
+        Console.WriteLine("Привет ");
+        Console.WriteLine("Мир");
+        Console.WriteLine("!!!");
+
+        //Цикл с проверкой кнопки "Enter". Текст в консоль не выводится. Я не делал с помощью ReadLine, так как тогда вводимые символы выводятся в консоль.
+        //С ReadLine мне это не понравилось, а как ограничить ввод пока не знаю.
+        while (Console.ReadKey(true).Key != ConsoleKey.Enter) ;
+        //Console.ReadLine();
+    }
+
+}
